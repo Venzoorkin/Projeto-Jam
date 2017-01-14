184f2e1fb3840a8d32a20fce18a35404409b371c
Gabriel Soares <venzoorkin@gmail.com>
2017-01-14 06:23:52 -0200

    Movimento para direçao do mouse pronto

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public Vector2 mousePos; //Posição do mouse
+    private PlayerController controller; //Controller do gameObject do jogador
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponent<PlayerController>(); //controller da variavel é o controller do GameObject
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && controller.inTransit == false)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            StartCoroutine(controller.MoveTo(mousePos));
+        }
+        else if (Input.GetMouseButtonUp(0) && controller.inTransit == true)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            controller.StopAllCoroutines();
+            controller.StartCoroutine(controller.MoveTo(mousePos));
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private bool reached;
+    public bool inTransit;
+    IEnumerator Moving(Vector3 target)
+    {
+        reached = false;
+        while (reached == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            inTransit = true;
+            yield return null;
+        }
+    }
+
+    public IEnumerator MoveTo(Vector2 target)
+    {
+        StartCoroutine(Moving(target));
+        yield return new WaitUntil(() => transform.position == new Vector3(target.x,target.y));
+        reached = true;
+        inTransit = false;
+        StopAllCoroutines();
+        yield return null;
+    }
+
+}
